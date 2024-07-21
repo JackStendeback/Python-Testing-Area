645efb62035c98c9600b7b6e66b7c951414a15fa
Jack Stendeback <jackstendeback@gmail.com>
2024-07-20 23:43:27 -0500

    minor bug fixes to coffee shop code

--- coffee_shop.py
+++ coffee_shop.py
@@ -6,22 +6,19 @@ def get_ingredients(drink):
     if drink in MENU:
         ingredients = MENU[drink]["ingredients"]
         water = ingredients.get("water", "no water found")
-        milk = ingredients.get("milk", "no milk found")
+        milk = ingredients.get("milk", "0")
         coffee = ingredients.get("coffee", "no coffee found")
         price = MENU[drink]["cost"]
-        return {
-            print(f"A {drink} has {water}oz of water, {milk}oz of milk and {coffee}oz of coffee. Cost: ${price}")
-        }
+        print(f"A {drink} has {water}oz of water, {milk}oz of milk and {coffee}oz of coffee. Cost: ${price}")
 
 
 # TODO: Welcome user to the coffee shop, and ask them what drink they would like to order.
 print("Welcome to Jack's Coffee Shop!")
 print("Here is a look at our menu.")
-print(get_ingredients('espresso'))
-print(get_ingredients('latte'))
-print(get_ingredients('cappuccino'))
+get_ingredients('espresso')
+get_ingredients('latte')
+get_ingredients('cappuccino')
 
 user_selection = input("'Espresso'. 'Latte', or 'Cappuccino'?: ").lower()
-
 
 # TODO: Make sure shop has sufficient ingredients to make the drink.
