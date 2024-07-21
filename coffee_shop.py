7b1a7962cc70fe7217c1f257b47eeb474db3a818
Jack Stendeback <jackstendeback@gmail.com>
2024-07-21 00:10:51 -0500

    put menu file inside main coffee shop file, starting code over so its much cleaner and refined

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,24 +1,31 @@
-from coffee_shop_menu import MENU
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-
-# TODO: Need a function to get the water, milk, coffee, and price values from the data set.
-def get_ingredients(drink):
-    if drink in MENU:
-        ingredients = MENU[drink]["ingredients"]
-        water = ingredients.get("water", "no water found")
-        milk = ingredients.get("milk", "0")
-        coffee = ingredients.get("coffee", "no coffee found")
-        price = MENU[drink]["cost"]
-        print(f"A {drink} has {water}oz of water, {milk}oz of milk and {coffee}oz of coffee. Cost: ${price}")
-
-
-# TODO: Welcome user to the coffee shop, and ask them what drink they would like to order.
-print("Welcome to Jack's Coffee Shop!")
-print("Here is a look at our menu.")
-get_ingredients('espresso')
-get_ingredients('latte')
-get_ingredients('cappuccino')
-
-user_selection = input("'Espresso'. 'Latte', or 'Cappuccino'?: ").lower()
-
-# TODO: Make sure shop has sufficient ingredients to make the drink.
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
