33ec206bef6b34a5a53221c492f97220ae2f757b
Jack Stendeback <jackstendeback@gmail.com>
2024-07-22 02:06:53 -0500

    completed coffee shop code

--- coffee_shop.py
+++ coffee_shop.py
@@ -33,6 +33,45 @@ resources = {
 # Currency value for the user.
 money = 0
 
+
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True if payment was accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global money
+        money += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️")
+
+
 # Variable used to control the on/off power of the while loop controlling our coffee shop, and to have an off switch.
 is_on = True
 # While loop that prompts the user for a drink choice, shows funds/resources, and ends the transaction when needed.
@@ -45,5 +84,14 @@ while is_on:
         print(f"Milk: {resources['milk']}")
         print(f"Coffee: {resources['coffee']}")
         print(f"Money: ${money}")
+    else:
+        drink = MENU[user_prompt]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(user_prompt, drink["ingredientsreport"])
+
+# TODO: When the user chooses a drink, the program should check if there are enough resources to make that drink.
+
 
 # TODO: Step 4 of the Coffee Shop checklist is next, lets complete it!
