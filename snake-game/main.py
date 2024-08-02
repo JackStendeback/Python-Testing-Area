bbbe1df5d7d3cb7e0ba630fc41675f1753cfe870
Jack Stendeback <jackstendeback@gmail.com>
2024-08-01 23:57:42 -0500

    minor comments

--- snake-game/main.py
+++ snake-game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 from snake import Snake
 import time
 
+# Setting up the screen for the game
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
